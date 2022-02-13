b7d5c8506b42f394e7d70461ee2b4c3be28e1856
Miyayu <yuchan2215@gmail.com>
2022-02-13 13:06:36 +0900

    change: batファイルを修正（ディレクトリも含められるようにした）

--- makezip.bat
+++ makezip.bat
@@ -1,8 +1,32 @@
-rmdir temp
+@REM 現在のディレクトリを変数に入れる
+set current_dir=%~dp0
+
+@REM 親ディレクトリへ移動
+cd ./../
+
+@REM tempフォルダを空にする
+rmdir /s /q temp
 mkdir temp
-copy * temp
+
+@REM 全てのファイルをコピー
+xcopy %current_dir% temp /D /S /R /Y /I /K
+
+@REM tempフォルダに移動
 cd temp
+
+@REM パッケージに同梱しないファイルを削除する
 del makezip.bat
 del autolive.zip
 del .gitignore
-"C:\Program Files\7-Zip\7z.exe" a ./../autolive.zip *
+rmdir /s /q ".git"
+rmdir /s /q ".idea"
+
+@REM 古いパッケージを削除する
+del %current_dir%autolive.zip
+
+@REM パッケージの作成
+"C:\Program Files\7-Zip\7z.exe" a %current_dir%autolive.zip *
+
+@REM tempフォルダを空にする
+cd ./../
+rmdir /s /q temp
