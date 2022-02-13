6e476d41acb2f497b389e3cde61cd9ad1f2c3b94
Miyayu <yuchan2215@gmail.com>
2022-02-13 16:34:11 +0900

    Revert "clean: format"
    This reverts commit 023f214b326f714941d0781bce5ec7af28877d81.

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.msg === "close") {
-        chrome.tabs.query({title: message.title}, function (result) {
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+    if(message.msg === "close"){
+        chrome.tabs.query({title:message.title},function(result){
             for (let i = 0; i < result.length; i++) {
-                chrome.tabs.remove(result[i].id).then()
+                chrome.tabs.remove(result[i].id)
             }
         })
     }
